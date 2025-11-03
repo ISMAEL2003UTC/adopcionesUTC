ec553d029ffab52f85061d2c3249a1d6b65a4254
giovanny.agama6161 <giovanny.agama6161@utc.edu.ec>
2025-11-03 10:30:53 -0500

    subiendo proyecto

---
+++ proyecto_adopciones/Aplicaciones/adopciones/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('persona/', views.persona, name='persona'),
+    path('mascota/', views.mascota, name='mascota'),
+    path('adopcion/', views.adopcion, name='adopcion'),
+    path('nuevaAdopcion/', views.nuevaAdopcion, name='nuevaAdopcion'),
+]
---
+++ proyecto_adopciones/Aplicaciones/adopciones/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .models import Persona, Mascota, Adopcion
+
+# Create your views here.
+# renderizando el listado de adopciones
+def persona(request):
+    listadoPersona = Persona.objects.all()
+    return render(request, 'persona.html', {'personas': listadoPersona})
+def mascota(request):
+    listadoMascota = Mascota.objects.all()
+    return render(request, 'mascota.html', {'mascotas': listadoMascota})
+def adopcion(request):
+    listadoAdopcion = Adopcion.objects.all()
+    return render(request, 'adopcion.html', {'adopciones': listadoAdopcion})
+
+
+def nuevaAdopcion(request):
+    return render(request, 'nuevaAdopcion.html')
